21921ecc446e1d10603c8c32aa470ed6f510b341
alex watson <alexwatsondemon@gmail.com>
2024-04-16 14:51:40 -0700

    made code
    made code and worked most errors out
    Please enter the commit message for your changes. Lines starting

--- nmsworthit.c
+++ nmsworthit.c
@@ -1,17 +1,57 @@
 #include <stdio.h>
 
-int main (){
-	if(){
-		if(){
+int main (int argc, char *argv[]){
+	if(argv[1] == "main"){
+		int a;
+		a = atoi(argv[2]);
+		if(a<4){
+			printf("need 20");
 		}
-		else if(){
+		else if(a<8){
+			printf("need 21");
 		}
+		else if(a<15){
+			printf("need 22");
+		}
+		else if(a<25){
+			printf("need 24");
+		}
+		else if(a<30){
+			printf("need 25");
+		}
+		else if(a<35){
+			printf("need 27");
+		}
+		else if(a<40){
+			printf("need 29");
+		}
+		else if(a<45){
+			printf("need 30");
+		}
+		else if(a<50){
+			printf("need 32");
+		}
+		else if(a<55){
+			printf("need 33");
+		}
+		else if(a>55){
+			printf("need 35");
+		}
+		else {
+			printf("provide number of expeditions");
+		}}}else if(argv[1] == "support"){
+		int a;
+		a = atoi(argv[2]);
+		if(a<4){
+			printf("need 2");
+		}
+		else if(a<8){
+			printf("need 4");
+		}
+		else {
+			printf("provide number of expeditions");
 	}
-	else if(){
-		if(){
-		}
-		else if(){
-		}
+	else {
+		printf("type main or support \n");
 	}
-	else(){
 	}
