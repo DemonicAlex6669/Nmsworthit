b39f2d94e0731eb0baaeac10ff881fa448688c2a
alex watson <alexwatsondemon@gmail.com>
2024-04-19 14:15:45 -0700

    wrote and compiled
    need to fix, only works for first value in chart

--- nmsworthit.c
+++ nmsworthit.c
@@ -1,64 +1,80 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main (int argc, char *argv[]){
-	typedef struct chart
-	{
-	int expedition;
-	int stat;
-	struct chart *next;
-	} chart;
+	if(strcmp(argv[1], "main") == 0){
 
-	chart *list = NULL;
-
-	int expd_value[11] = 4, 8, 15, 25, 30, 35, 40, 45, 50, 55, 100 ;
-	int stat_value[11] = 20, 21, 22, 24, 25, 27, 29, 30, 32, 33, 35 ;
-	
-	for (int i=0; i<11; i++)
-	{
-	int x = expd_value[i];
-	int y = stat_value[i];
-
-	chart *n = malloc(sizeof(chart));
-		if (n == NULL)
+		typedef struct chart
 		{
-		return 1;
-		}
-	n->expedition = x;
-	n->stat = y;
-	n->next = NULL;
-	}
-	if (list == NULL)
-	{
-	list = n;
-	}
-	else
-	{
-		for (chart *ptr = list; ptr != NULL;ptr =  ptr->next)
+		int expedition;
+		int stat;
+		struct chart *next;
+		} chart;
+
+		chart *list = NULL;
+
+		int expd_value[11] = {4, 8, 15, 25, 30, 35, 40, 45, 50, 55, 100} ;
+		int stat_value[11] = {20, 21, 22, 24, 25, 27, 29, 30, 32, 33, 35} ;
+		
+		for (int i=0; i<11; i++)
 		{
-			if (ptr->next == NULL)
+		int x = expd_value[i];
+		int y = stat_value[i];
+
+		chart *n = malloc(sizeof(chart));
+			if (n == NULL)
 			{
-			ptr->next = n;
-			break;
+			return 1;
+			}
+		n->expedition = x;
+		n->stat = y;
+		n->next = NULL;
+		
+			if (list == NULL)
+			{
+			list = n;
+			}
+			else
+			{
+				for (chart *ptr = list; ptr != NULL;ptr =  ptr->next)
+				{
+					if (ptr->next == NULL)
+					{
+					ptr->next = n;
+					break;
+					}
+				}
+
 			}
 		}
 
-	}
 
-
-        for (chart *pt =list; pt != NULL; pt =  pt->next)
-	{
-	int i = 0;
-	int x = expd_value[i];
-        int y = stat_value[i];
-		if ( pt->expedition > x) 
+		for (chart *pt =list; pt != NULL; pt =  pt->next)
 		{
-		printf("good buy\n");
-		}
-		else
-		{
-
-		}
+		int i = 0;
+		int x = expd_value[i];
+		int y = stat_value[i];
+		int a;
+		a = atoi(argv[2]);
+		int b;
+		b = atoi(argv[3]);
+			if ( x > a) 
+			{
+				if (b >= y)
+				{
+				printf("good buy\n");
+				}
+				else
+				{
+				printf("bad buy\n");
+				}
+			}
+			else
+			{
+			i++;
+			}
 	}
+}
      else if(strcmp(argv[1], "support") == 0){
 	int a;
 		a = atoi(argv[2]);
