0182a5f17ea4afd5bd5f8d7443c4f7ada6856c67
alex watson <alexwatsondemon@gmail.com>
2024-04-20 00:24:00 -0700

    bug free, finished

--- nmsworthit.c
+++ nmsworthit.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 
 int main (int argc, char *argv[]){
-	if(strcmp(argv[1], "main") == 0){
+	if(strcmp(argv[1], "main") == 0)
+	{
 
 		typedef struct chart
 		{
@@ -51,43 +52,102 @@ int main (int argc, char *argv[]){
 
 		for (chart *pt =list; pt != NULL; pt =  pt->next)
 		{
-		int i = 0;
-		int x = expd_value[i];
-		int y = stat_value[i];
 		int a;
 		a = atoi(argv[2]);
 		int b;
 		b = atoi(argv[3]);
-			if ( x > a) 
+			if ( pt->expedition > a) 
 			{
-				if (b >= y)
+				if (pt->stat <= b )
 				{
 				printf("good buy\n");
+				return 0;
 				}
-				else
+				else if (pt->next == NULL)
 				{
 				printf("bad buy\n");
+				return 0;
 				}
+			}
+		}
+	}
+
+     else if(strcmp(argv[1], "support") == 0)
+	{
+	 
+		typedef struct chart
+		{
+		int expedition;
+		int stat;
+		struct chart *next;
+		} chart;
+
+		chart *list = NULL;
+
+		int expd_value[11] = {4, 8, 15, 25    , 30, 35, 40, 45, 50, 55, 100 };
+		int stat_value[11] = {2, 4, 5, 6, 8, 9, 10, 11, 12, 13, 15} ;
+		
+		for (int i=0; i< 11; i++)
+		{
+		int x = expd_value[i];
+		int y = stat_value[i];
+
+		chart *n = malloc(sizeof(chart));
+			if (n == NULL)
+			{
+			return 1;
+			}
+		n->expedition = x;
+		n->stat = y;
+		n->next = NULL;
+		
+			if (list == NULL)
+			{
+			list = n;
 			}
 			else
 			{
-			i++;
+				for (chart *ptr = list; ptr != NULL;ptr =  ptr->next)
+				{
+					if (ptr->next == NULL)
+					{
+					ptr->next = n;
+					break;
+					}
+				}
 			}
+
+		}
+
+
+		for (chart *pt =list; pt != NULL; pt =  pt->next)
+		{
+		int a;
+		a = atoi(argv[2]);
+		int c;
+		int d;
+		int e;
+		int f;
+		c = atoi(argv[3]);
+		d = atoi(argv[4]);
+		e = atoi(argv[5]);
+		f = atoi(argv[6]);
+		float b;
+		b = ((float)c + (float)d + (float)e + (float)f)/4;
+		
+			if ( pt->expedition > a) 
+			{
+				if (pt->stat <= b )
+				{
+				printf("good buy\n");
+				return 0;
+				}
+				else if (pt->next == NULL)
+				{
+				printf("bad buy\n");
+				return 0;
+				}
+			}
+		}
 	}
 }
-     else if(strcmp(argv[1], "support") == 0){
-	int a;
-		a = atoi(argv[2]);
-		if(a<4){
-			printf("need 2\n");
-		}
-		else if(a<8){
-			printf("need 4\n");
-		}
-		else {
-			printf("provide number of expeditions\n");
-	}}
-	else {
-		printf("type main or support \n");
-	}
-	}
