e0a630834afeecf53b296bb7b6b55158028bd260
alex watson <alexwatsondemon@gmail.com>
2025-02-17 23:01:26 -0800

    outline of python version

---
+++ nmsworthit.py
@@ -0,0 +1,24 @@
+import argparse
+
+class ship(...):
+    def __init__:
+        ...
+    @property
+    def status(self):
+        ...
+    @status.setter
+    def status(self,...):
+        ...
+    @classmethod
+    def worth(...):
+        ...
+
+def main():
+    parser = argparse.ArgumentParser(description = "find of a ship should be bought")
+    parser.add_argument()
+    args = parser.parse_args()
+
+    ...
+
+if __name__ == "__main__":
+    main()
