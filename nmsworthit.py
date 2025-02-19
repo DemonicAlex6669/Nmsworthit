f4e2fa6ac17a55b1ac5834b220d8b02d5b2c5f58
alex watson <alexwatsondemon@gmail.com>
2025-02-19 12:09:29 -0800

    layed out logic

--- nmsworthit.py
+++ nmsworthit.py
@@ -1,17 +1,16 @@
 import argparse
 
-class ship(...):
-    def __init__:
-        ...
-    @property
-    def status(self):
-        ...
-    @status.setter
-    def status(self,...):
-        ...
+class ship:
+    def __init__(srlf, **chart):
+        self._chart = chart
     @classmethod
     def worth(...):
-        ...
+        for key, value in self._chart.items():
+            if args.e > key:
+                if ...:
+                    ...
+                ...
+            ...
 
 def main():
     parser = argparse.ArgumentParser(description = "find of a ship should be bought")
@@ -20,7 +19,18 @@ def main():
     paeser.add_argument("-s", nargs=4, help= "the four stats for a support ship")
     args = parser.parse_args()
 
-    ...
+    stat = 0
+
+    for s in args.s:
+        stats += s
+        stat = stats / 4
+        return stat
+
+    if stat = 0:
+        stat = args.m
+        return stat
+
+    print(worth())
 
 if __name__ == "__main__":
     main()
